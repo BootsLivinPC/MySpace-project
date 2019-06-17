8be866bd5c31ea563969c8acc9e0399debf00fad
Chris McGivney <cmmcgivney84@gmail.com>
2019-06-17 15:04:12 -0600

    intial auth commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Fragment, } from 'react';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import Register from './components/Register';
+import { Switch, Route, } from 'react-router-dom';
+import { Container, } from "semantic-ui-react";
+import FetchUser from './components/FetchUser'
+import ProtectedRoute from './components/ProtectedRoute'
+
+
+const App = () => (
+  <Fragment>
+    <NavBar />
+    <FetchUser>
+     <Container>
+      <Switch>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+     </Container>
+    </FetchUser>
+  </Fragment>
+)
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router } from "react-router-dom"
+import { AuthProvider } from './providers/AuthProvider';
+import "semantic-ui-css/semantic.min.css"
+import { initMiddleware, } from 'devise-axios'
+
+initMiddleware()
+
+ReactDOM.render(
+  <AuthProvider>
+    <Router>
+      <App />
+    </Router>
+  </AuthProvider>,
+document.getElementById('root'));
+
+
+serviceWorker.unregister();
